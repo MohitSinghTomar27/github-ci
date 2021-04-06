bd7be0357fe0535934eba24d70c5115b61a766fd
MohitSinghTomar27 <mtmohit59@gmail.com>
2021-04-06 19:39:55 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Mohit To Heroku
         </p>
         <a
           className="App-link"
